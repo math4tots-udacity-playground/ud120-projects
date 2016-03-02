72ff52bcd5b5daea2623b85989589124ff8dfc14
Kyumin Kim <math4tots@gmail.com>
2016-03-02 12:55:17 -0800

    Lesson 3: Decision Trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+print "num features = ", len(features_train[0])
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+accuracy = accuracy_score(clf.predict(features_test), labels_test)
+print accuracy
 
 #########################################################
 
